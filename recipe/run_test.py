64301eb38870e3f11f662ffd36f2d1a0679d5da8
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-11-25 10:08:36 +0100

    add cupti tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -10,7 +10,7 @@ from numba.cuda.cudadrv.nvvm import NVVM
 
 def run_test():
     # on windows only nvvm is available to numba
-    if sys.platform.startswith('win'):
+    if sys.platform.startswith("win"):
         nvvm = NVVM()
         print("NVVM version", nvvm.get_version())
         return nvvm.get_version() is not None
@@ -18,11 +18,16 @@ def run_test():
         return False
     nvvm = NVVM()
     print("NVVM version", nvvm.get_version())
-    # check pkg version matches lib pulled in
-    gotlib = get_cudalib('cublas')
-    # check cufft b/c cublas has an incorrect version in 10.1 update 1
-    gotlib = get_cudalib('cufft')
-    return bool(get_cudalib('cublas')) and bool(get_cudalib('cufft'))
+
+    extra_lib_tests = (
+        "cublas",  # check pkg version matches lib pulled in
+        "cufft",  # check cufft b/c cublas has an incorrect version in 10.1 update 1
+        "cupti",  # check this is getting included
+    )
+    found_paths = [get_cudalib(lib) for lib in extra_lib_tests]
+    print(*zip(extra_lib_tests, found_paths), sep="\n")
+
+    return all(extra_lib_tests)
 
 
 sys.exit(0 if run_test() else 1)
